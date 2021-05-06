76a009b2972ab97be567e577fcebb72ad69fdaf7
LOK <80965262+lokpavan03@users.noreply.github.com>
2021-05-06 12:36:32 +0530

    Update outputs.tf

--- config/outputs.tf
+++ config/outputs.tf
@@ -1,4 +1,4 @@
 #Instance IP
 output "instance_ip_addr" {
-  value = azurerm_linux_virtual_machine.myterraformvm.private_ip_address
+  value = azurerm_linux_virtual_machine.myterraformvm.public_ip_address
 }
